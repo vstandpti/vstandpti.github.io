1b610fd863b6f39b1c1ec834c9647ee88957ebf0
vstandpti <43736611+vstandpti@users.noreply.github.com>
2018-10-02 12:00:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,25 +10,4 @@ function myMap() {
     content: "V'STAND ADA DI SINI!"
   });
   infowindow.open(map,marker);
-  
-  // Scroll to specific values
-// scrollTo is the same
-window.scroll({
-  top: 2500, 
-  left: 0, 
-  behavior: 'smooth' 
-});
-
-// Scroll certain amounts from current position 
-window.scrollBy({ 
-  top: 100, // could be negative value
-  left: 0, 
-  behavior: 'smooth' 
-});
-
-// Scroll to a certain element
-document.querySelector('.hello').scrollIntoView({ 
-  behavior: 'smooth' 
-});
-  
 }
