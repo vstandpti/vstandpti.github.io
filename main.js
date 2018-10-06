b87543b1e8a699ef4ea634a810ac65614a4ef89e
vstandpti <43736611+vstandpti@users.noreply.github.com>
2018-10-06 08:53:09 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -19,21 +19,62 @@
 // function generateUserTable(data){
 //   console.log(data);
 //   let idx = 0
-//   let tbody = document.getElementById('quote').innerHTML = ''
+//   let tbody = document.getElementById('tableKritik').innerHTML = ''
 //   for(idx = 0; idx < data.length; idx++){
-//     let singleRow = document.createElement('div')
-//     let colName = document.createElement('cite')
+//     let singleRow = document.createElement('tr')
+//     let colName = document.createElement('td')
 //     colName.appendChild(document.createTextNode(data[idx].name))
-//     let colNim = document.createElement('cite')
-//     colEmail.appendChild(document.createTextNode(data[idx].nim))
-//     let colQuote = document.createElement('blockquote')
-//     colWebsite.appendChild(document.createTextNode(data[idx].quote))
+//     let colMessage = document.createElement('td')
+//     colMessage.appendChild(document.createTextNode(data[idx].quote))
 
-//     singleRow.appendChild(colName)
-//     singleRow.appendChild(colNim)   
-//     singleRow.appendChild(colQuote)
+//     singleRow.appendChild(colName) 
+//     singleRow.appendChild(colMessage)
 //   }
 // }
+
+// function loadUserData(){
+//     function generateUserTable(data){
+//     function loadAlbum(){
+//             let request = new XMLHttpRequest()
+//     request.open('GET', 'quote.json', true)
+
+//     request.onload = function() {
+//         if (request.status >= 200 && request.status < 400) {
+//             let data = JSON.parse(request.responseText)
+//             generateUserTable(data);
+//         } else {
+//             alert('Page Not Found!')
+//         }
+//     }
+//     request.onerror = function() {
+//         alert('Request Failed! Check your internet connection!')
+//     }
+
+//     request.send()
+//     }
+
+//     let idx = 0;
+//     let tbody = document.getElementById('tableKritik').querySelector('tbody')
+//     tbody.innerHTML = ''
+//     for(idx=0;idx<data.length;idx++) {
+//         let singleRow = document.createElement('tr')
+
+//         let colName = document.createElement('td')
+//         colName.appendChild(document.createTextNode(data[idx].name))
+//         let colMessage = document.createElement('td')
+//         colMessage.appendChild(document.createTextNode(data[idx].quote))
+
+//         singleRow.appendChild(colName)
+//         singleRow.appendChild(colMessage)
+//         tbody.appendChild(singleRow)
+//     }
+// }
+// }
+
+// function onDocumentFinish(){
+//     loadUserData()
+// }
+
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -70,13 +111,15 @@ function onDocumentFinish(){
             const messageCol = document.createElement("td");
             messageCol.appendChild(document.createTextNode(item['message']));
             
-            newRow.appendChild(nameCol);
             newRow.appendChild(messageCol);
+            newRow.appendChild(nameCol);
+            
 
             tbody.appendChild(newRow);
 
-            createdBy.elements['itemName'].value = ' ';
             createdBy.elements['itemMessage'].value = ' ';
+            createdBy.elements['itemName'].value = ' ';
+            
 
             // Total = document.getElementById('tableItem').rows.length;
             // document.getElementById('total').innerHTML = "Jumlah data orang sekarang ada : " + (Total-1);
